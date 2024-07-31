c27d75de9e416c0a5e28ddf9c4ce3239f5a12989
haakontveit1 <152721186+haakontveit1@users.noreply.github.com>
2024-07-31 15:00:52 +0200

    biiig

--- waterfall_slakt_streamlit.py
+++ waterfall_slakt_streamlit.py
@@ -5,7 +5,7 @@ Created on Wed Jul 31 08:53:23 2024
 @author: HåkonTveiten
 """
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
@@ -61,29 +61,37 @@ def velg_dato():
     valgt_dato = datetime(år, maaned, dag)
     return valgt_dato
 
+def hent_uke_dager(år, uke_nummer):
+    start_dato = datetime.strptime(f'{år}-W{uke_nummer-1}-1', "%Y-W%W-%w")
+    return [start_dato + timedelta(days=i) for i in range(5)]  # Returner mandag-fredag
+
 def main():
     st.title("Produksjonsanalyse")
     
-    # Velg type ark
+    # Velg type analyse
+    analyse_type = st.selectbox("Velg analyse:", ["Spesifikk dato", "Hele uken"])
     sheet_type = st.selectbox("Velg type ark:", ["slakt", "filet"])
     oee_100 = 150 if sheet_type == "slakt" else 25
     stiplet_hoeyde = 120 if sheet_type == "slakt" else 20
     
-    # Filopplastingsseksjon
+    if analyse_type == "Spesifikk dato":
+        valgt_dato = velg_dato()
+    else:
+        år = st.number_input("Velg år:", min_value=2024, max_value=datetime.now().year)
+        uke_nummer = st.number_input("Velg uke nummer:", min_value=1, max_value=52)
+        uker_dager = hent_uke_dager(år, uke_nummer)
+    
     uploaded_file = st.file_uploader(f"Velg en Excel-fil (må være et 'input-{sheet_type}'-ark).", type=["xlsx"])
     
     if uploaded_file is None:
         st.warning("Vennligst last opp en Excel-fil for å fortsette.")
         return
 
-    # Last inn data fra opplastet fil
     df = les_data(uploaded_file)
     
     if df is None:
         st.warning("Ingen data tilgjengelig i den opplastede filen. Vennligst last opp en gyldig Excel-fil.")
         return
-
-    valgt_dato = velg_dato()
 
     # Konverter første kolonne til datetime-format
     try:
@@ -92,13 +100,7 @@ def main():
         st.error(f"Feil ved konvertering av dato: {e}")
         st.write("Det kan være et problem med datoformatet i filen, eller du har lastet opp feil type fil.")
         return
-    
-    # Sjekk om valgt dato finnes i første kolonne
-    valgt_dato_str = valgt_dato.strftime("%d.%m.%Y")  # Konverterer til ønsket format
-    st.write(f"Valgt dato: {valgt_dato_str}")
-    st.write("----------------------------------")
-    st.write("Statistikk")
-    
+
     # Filtrer datoene for å sammenligne basert på år, måned og dag
     try:
         df['Dato'] = df.iloc[:, 0].dt.date
@@ -107,120 +109,85 @@ def main():
         st.write("Det kan være et problem med strukturen på den opplastede filen.")
         return
 
-    valgt_dato_enkel = valgt_dato.date()
-    
-    if valgt_dato_enkel in df['Dato'].values:
-        row = df[df['Dato'] == valgt_dato_enkel].iloc[0]
-
-        stopptid = beregn_stopptid(row, sheet_type)
-        if stopptid is None:
-            st.error("Kan ikke beregne stopptid. Sjekk om du har valgt riktig ark type (file eller slakt) og lastet opp riktig fil.")
-            return
-        
-        stopptid_impact = stopptid * oee_100
-        stopptid_takt = round(stopptid_impact / 60 / 8, 2)
-        
-        arbeidstimer, antall_fisk = beregn_faktiskproduksjon(row, sheet_type)
-        if arbeidstimer is None or antall_fisk is None:
-            st.error("Kan ikke beregne faktisk produksjon. Sjekk om du har valgt riktig filtype og lastet opp riktig fil.")
-            return
-        
-        # Define the width for alignment
-        label_width = 70  # Adjust this value based on your label lengths
-        value_width = 70  # Adjust this value based on your value lengths
-
-        faktisk_takt = round(antall_fisk / arbeidstimer, 2)
-        kjente_faktorer = round(stopptid_takt, 2)
-        annet = oee_100 - kjente_faktorer - faktisk_takt
-        annet = round(annet, 2)
-
-        # Define the formatted output using HTML
-        output_html = f"""
-        <table style="width:100%; border-spacing: 0;">
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">OEE 100%:</td>
-            <td style="text-align:right; width:30%;">{oee_100} fisk/minutt</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Total stopptid:</td>
-            <td style="text-align:right; width:30%;">{round(stopptid, 2)} minutter</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Tapt takt per minutt på grunn av stopp:</td>
-            <td style="text-align:right; width:30%;">{stopptid_takt} fisk/minutt</td>
-        </tr>
-        </table>
-        <br>  <!-- Tomme linjer for å skille seksjonene -->
-        <table style="width:100%; border-spacing: 0;">
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Arbeitstimer:</td>
-            <td style="text-align:right; width:30%;">{round(arbeidstimer/60, 2)} timer</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Antall fisk produsert:</td>
-            <td style="text-align:right; width:30%;">{antall_fisk} fisk</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Antall fisk tapt pga stopptid:</td>
-            <td style="text-align:right; width:30%;">{round(stopptid_impact, 2)} fisk</td>
-        </tr>
-        </table>
-        <br>
-        <table style="width:100%; border-spacing: 0;">
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Annet tap (unoterte feil, operatørhastighet etc):</td>
-            <td style="text-align:right; width:30%;">{annet} minutter</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Faktisk takt:</td>
-            <td style="text-align:right; width:30%;">{faktisk_takt} fisk/minutt</td>
-        </tr>
-        <tr>
-            <td style="text-align:left; width:70%; padding-right: 10px;">Avstand fra 80% OEE takttid:</td>
-            <td style="text-align:right; width:30%;">{round(stiplet_hoeyde - faktisk_takt, 2)} fisk/minutt</td>
-        </tr>
-        </table>
-        """
-
-        # Display the output in Streamlit using st.markdown
-        st.markdown(output_html, unsafe_allow_html=True)
-
-        # Data for waterfall grafen
-        stages = ['100% OEE', 'Stopptid', 'Annet']
-        values = [oee_100, -stopptid_takt, -annet]
-
-        cum_values = np.cumsum([0] + values).tolist()
-        value_starts = cum_values[:-1]
-
-        # Plot waterfall grafen
-        fig, ax = plt.subplots()
-        colors = ['blue', 'red', 'orange']
-
-        for i in range(len(stages)):
-            ax.bar(stages[i], values[i], bottom=value_starts[i], color=colors[i], edgecolor='black')
-
-        ax.bar('Takttid', faktisk_takt, bottom=0, color='green', edgecolor='black')
-        ax.bar('Takttid', stiplet_hoeyde - faktisk_takt, bottom=faktisk_takt, color='none', edgecolor='green', hatch='//')
-
-        for i in range(len(stages)):
-            y_pos = value_starts[i] + values[i] / 2
-            ax.text(stages[i], y_pos, f'{values[i]}', ha='center', va='center', color='white', fontweight='bold')
-
-        ax.text('Takttid', faktisk_takt / 2, f'{faktisk_takt}', ha='center', va='center', color='white', fontweight='bold')
-        ax.text('Takttid',faktisk_takt + (stiplet_hoeyde - faktisk_takt) / 2, f'{round(stiplet_hoeyde - faktisk_takt, 2)}', ha='center', va='center', color='green', fontweight='bold')
-
-        fisk = "fisk"
-        pa = " (på slakt)"
-        if sheet_type == "filet":
-            fisk = "fileter"
-            pa = ""
-
-        ax.set_ylabel(f'Antall {fisk} produsert per minutt')
-        ax.set_title(f'Antall {fisk} produsert per minutt {valgt_dato_str}{pa}')
-        st.pyplot(fig)
-
+    if analyse_type == "Spesifikk dato":
+        valgt_dato_enkel = valgt_dato.date()
+        if valgt_dato_enkel in df['Dato'].values:
+            row = df[df['Dato'] == valgt_dato_enkel].iloc[0]
+            stopptid = beregn_stopptid(row, sheet_type)
+            arbeidstimer, antall_fisk = beregn_faktiskproduksjon(row, sheet_type)
+            if stopptid is None or arbeidstimer is None or antall_fisk is None:
+                st.error("Kan ikke beregne verdier. Sjekk om du har valgt riktig filtype og lastet opp riktig fil.")
+                return
+            
+            stopptid_impact = stopptid * oee_100
+            stopptid_takt = round(stopptid_impact / 60 / 8, 2)
+            faktisk_takt = round(antall_fisk / arbeidstimer, 2)
+            kjente_faktorer = round(stopptid_takt, 2)
+            annet = oee_100 - kjente_faktorer - faktisk_takt
+            annet = round(annet, 2)
+            
+            # Plotting single day data
+            fig, ax = plt.subplots()
+            ax.bar(['100% OEE', 'Stopptid', 'Annet'], [oee_100, -stopptid_takt, -annet], color=['blue', 'red', 'orange'])
+            ax.bar('Takttid', faktisk_takt, bottom=0, color='green', edgecolor='black')
+            ax.bar('Takttid', stiplet_hoeyde - faktisk_takt, bottom=faktisk_takt, color='none', edgecolor='green', hatch='//')
+            ax.set_ylabel(f'Antall {fisk} produsert per minutt')
+            ax.set_title(f'Antall {fisk} produsert per minutt {valgt_dato.strftime("%d.%m.%Y")}{pa}')
+            st.pyplot(fig)
+        else:
+            st.warning("Datoen du valgte finnes ikke i input-arket. Dette er enten fordi du tastet inn en ugyldig dato eller fordi datoen ikke hadde noen produksjon (eks helg).")
     else:
-        st.warning("Datoen du valgte finnes ikke i input-arket. Dette er enten fordi du tastet inn en ugyldig dato eller fordi datoen ikke hadde noen produksjon (eks helg).")
+        daglig_data = []
+        for dag in uker_dager:
+            dag_enkel = dag.date()
+            if dag_enkel in df['Dato'].values:
+                row = df[df['Dato'] == dag_enkel].iloc[0]
+                stopptid = beregn_stopptid(row, sheet_type)
+                arbeidstimer, antall_fisk = beregn_faktiskproduksjon(row, sheet_type)
+                if stopptid is None or arbeidstimer is None or antall_fisk is None:
+                    st.error(f"Kan ikke beregne verdier for {dag.strftime('%d.%m.%Y')}. Sjekk om du har valgt riktig filtype og lastet opp riktig fil.")
+                    return
+                daglig_data.append((dag, stopptid, arbeidstimer, antall_fisk))
+
+        if not daglig_data:
+            st.warning("Ingen gyldige data funnet for den valgte uken.")
+            return
+
+        # Plotting for each day
+        fig, axes = plt.subplots(2, 3, figsize=(15, 10))
+        for i, (dag, stopptid, arbeidstimer, antall_fisk) in enumerate(daglig_data):
+            ax = axes.flatten()[i]
+            stopptid_impact = stopptid * oee_100
+            stopptid_takt = round(stopptid_impact / 60 / 8, 2)
+            faktisk_takt = round(antall_fisk / arbeidstimer, 2)
+            kjente_faktorer = round(stopptid_takt, 2)
+            annet = oee_100 - kjente_faktorer - faktisk_takt
+            annet = round(annet, 2)
+            
+            ax.bar(['100% OEE', 'Stopptid', 'Annet'], [oee_100, -stopptid_takt, -annet], color=['blue', 'red', 'orange'])
+            ax.bar('Takttid', faktisk_takt, bottom=0, color='green', edgecolor='black')
+            ax.bar('Takttid', stiplet_hoeyde - faktisk_takt, bottom=faktisk_takt, color='none', edgecolor='green', hatch='//')
+            ax.set_title(f'{dag.strftime("%d.%m.%Y")}', fontsize=10)
+
+        # Plotting weekly average data
+        avg_stopptid = np.mean([data[1] for data in daglig_data])
+        avg_arbeidstimer = np.mean([data[2] for data in daglig_data])
+        avg_antall_fisk = np.mean([data[3] for data in daglig_data])
+        avg_stopptid_impact = avg_stopptid * oee_100
+        avg_stopptid_takt = round(avg_stopptid_impact / 60 / 8, 2)
+        avg_faktisk_takt = round(avg_antall_fisk / avg_arbeidstimer, 2)
+        avg_kjente_faktorer = round(avg_stopptid_takt, 2)
+        avg_annet = oee_100 - avg_kjente_faktorer - avg_faktisk_takt
+        avg_annet = round(avg_annet, 2)
+        
+        ax = axes.flatten()[-1]
+        ax.bar(['100% OEE', 'Stopptid', 'Annet'], [oee_100, -avg_stopptid_takt, -avg_annet], color=['blue', 'red', 'orange'])
+        ax.bar('Takttid', avg_faktisk_takt, bottom=0, color='green', edgecolor='black')
+        ax.bar('Takttid', stiplet_hoeyde - avg_faktisk_takt, bottom=avg_faktisk_takt, color='none', edgecolor='green', hatch='//')
+        ax.set_title(f'Ukesnitt {år}-W{uke_nummer}', fontsize=10)
+
+        plt.tight_layout()
+        st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
