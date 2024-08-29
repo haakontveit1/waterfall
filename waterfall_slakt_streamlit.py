e1028a238b1586defabebcd62483c22d9b1538f5
haakontveit1 <152721186+haakontveit1@users.noreply.github.com>
2024-08-29 13:17:12 +0200

    Update waterfall_slakt_streamlit.py
    60

--- waterfall_slakt_streamlit.py
+++ waterfall_slakt_streamlit.py
@@ -137,7 +137,7 @@ def main():
             
             stopptid_impact = stopptid * oee_100
             st.write(arbeidstimer)
-            stopptid_takt = round(stopptid_impact / 60 / arbeidstimer, 2)
+            stopptid_takt = round(stopptid_impact / arbeidstimer, 2)
             faktisk_takt = round(antall_fisk / arbeidstimer, 2)
             kjente_faktorer = round(stopptid_takt, 2)
             annet = oee_100 - kjente_faktorer - faktisk_takt
